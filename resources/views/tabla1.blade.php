2361eaa268d075e46968dd1d26db6cb7931e04e4
Vikktor93 <viktorsaldivia93@gmail.com>
2019-07-29 23:01:06 -0400

    Cambio de Nombre

--- resources/views/tabla1.blade.php
+++ resources/views/tabla1.blade.php
@@ -7,7 +7,7 @@
                         @foreach($users as $user)
                         <ul>
                             <li class="list-group-item list-group-item-dark"><b>Nombre:</b> {{$user->Username}}</li>
-                            <li class="list-group-item list-group-item-dark"><b>RUT:</b> {{$user->RUT_usuario}}</li>
+                            <li class="list-group-item list-group-item-dark"><b>Rut:</b> {{$user->RUT_usuario}}</li>
                                 @foreach($user->subusers as $subuser)
                                <ul>
                                     <li class="list-group-item list-group-item-light"><b>Nombre:</b> {{$subuser->Username}}</li>
